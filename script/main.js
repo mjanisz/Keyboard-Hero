4d0ab2d665693324036c200d3cb0db158ff7cac5
Gene Evans <geevans@csumb.edu>
2018-04-07 00:28:12 -0700

    added new audio file names

--- script/main.js
+++ script/main.js
@@ -9,6 +9,18 @@ var app = new ENGINE.Application({
 	this.assets.addImage("bg.jpg");
 	this.assets.addImage("logo.png");
 	this.assets.addAudio("ratatat.mp3");
+  this.assets.addAudio("unicorn.mp3");
+  this.assets.addAudio("standbyme.mp3");
+  this.assets.addAudio("lostinfire.mp3");
+  this.assets.addAudio("foolmeonce.mp3");
+  this.assets.addAudio("glasstable.mp3");
+  this.assets.addAudio("childrenof.mp3");
+  this.assets.addAudio("mothersson.mp3");
+  this.assets.addAudio("newworld.mp3");
+  this.assets.addAudio("trumpofdoom.mp3");
+  this.assets.addAudio("dondraper.mp3");
+  this.assets.addAudio("breakingbad.mp3");
+  this.assets.addAudio("newworldremix.mp3");
 	console.log(this)
   },
 
