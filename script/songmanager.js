fd89cc4a5bd99ddddbd43604afd07f0b1518b7ad
Gene Evans <Gene.x.Evans.-ND@disney.com>
2018-04-10 11:05:31 -0700

    added new properties and note method calls to the list creation

--- script/songmanager.js
+++ script/songmanager.js
@@ -55,32 +55,156 @@ ENGINE.SongManager.prototype = {
     //TODO need to create new Lists for each song to match the music
     switch (index)
     {
-      case 0:
+      case 0: // ratatat (uses default values from list.js)
         return new ENGINE.List();
-      case 1:
-        return new ENGINE.List();
-      case 2:
-        return new ENGINE.List();
-      case 3:
-        return new ENGINE.List();
-      case 4:
-        return new ENGINE.List();
-      case 5:
-        return new ENGINE.List();
-      case 6:
-        return new ENGINE.List();
-      case 7:
-        return new ENGINE.List();
-      case 8:
-        return new ENGINE.List();
-      case 9:
-        return new ENGINE.List();
-      case 10:
-        return new ENGINE.List();
-      case 11:
-        return new ENGINE.List();
-      case 12:
-        return new ENGINE.List();
+
+
+
+
+      case 1: // Secret of the Unicorn Queen
+        return new ENGINE.List(
+          {
+            song: getUnicornNotes(),
+            promptDelay: 15200,
+            noteDelay: 11100,
+          }
+        );
+
+
+
+
+      case 2: // Stand by Me
+        return new ENGINE.List(
+          {
+            song: getStandByMeNotes(),
+            promptDelay: 15200,
+            noteDelay: 11100,
+          }
+        );
+
+
+
+
+      case 3: // Things We Lost in the Fire
+        return new ENGINE.List(
+          {
+            song: getLostInFireNotes(),
+            promptDelay: 15200,
+            noteDelay: 11100,
+          }
+        );
+
+
+
+
+      case 4: // Fool Me Once
+        return new ENGINE.List(
+          {
+            song: getFoolMeOnceNotes(),
+            promptDelay: 15200,
+            noteDelay: 11100,
+          }
+        );
+
+
+
+
+      case 5: // The Glass Table
+        return new ENGINE.List(
+          {
+            song: getGlassTableNotes(),
+            promptDelay: 15200,
+            noteDelay: 11100,
+          }
+        );
+
+
+
+
+      case 6: // Children of the Damned
+        return new ENGINE.List(
+          {
+            song: getChildrenOfNotes(),
+            promptDelay: 15200,
+            noteDelay: 11100,
+          }
+        );
+
+
+
+
+      case 7: // Every Mother's Son
+        return new ENGINE.List(
+          {
+            song: getMothersSonNotes(),
+            promptDelay: 15200,
+            noteDelay: 11100,
+          }
+        );
+
+
+
+
+      case 8: // Brave New World
+        return new ENGINE.List(
+          {
+            song: getNewWorldNotes(),
+            promptDelay: 15200,
+            noteDelay: 11100,
+          }
+        );
+
+
+
+
+      case 9: // Trump of Doom (Bonus Track)
+        return new ENGINE.List(
+          {
+            song: getTrumpOfDoomNotes(),
+            promptDelay: 15200,
+            noteDelay: 11100,
+          }
+        );
+
+
+
+
+      case 10: // Don Draper (Bonus Track)
+        return new ENGINE.List(
+          {
+            song: getDonDraperNotes(),
+            promptDelay: 15200,
+            noteDelay: 11100,
+          }
+        );
+
+
+
+
+      case 11: // Breaking Bad (Bonus Track)
+        return new ENGINE.List(
+          {
+            song: getBreakingBadNotes(),
+            promptDelay: 15200,
+            noteDelay: 11100,
+          }
+        );
+
+
+
+
+      case 12: // Brave New World Remix (Bonus Track)
+        return new ENGINE.List(
+          {
+            song: getNewWorldRemixNotes(),
+            promptDelay: 15200,
+            noteDelay: 11100,
+          }
+        );
+
+
+
+
       default:
         return null;
     }
